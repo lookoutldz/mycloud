1940f26556c3893e3f72853d818b8010d814cb25
looko <lookoutldz@outlook.com>
2023-07-19 20:26:34 +0800

    添加 jwt 工具类，更新版本号

---
+++ common-starter/src/main/java/org/looko/mycloud/commonstarter/util/JsonUtil.java
@@ -0,0 +1,25 @@
+package org.looko.mycloud.commonstarter.util;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class JsonUtil {
+
+    private static final ObjectMapper objectMapper = new ObjectMapper();
+
+    static public <T> String toJson(T t) {
+        try {
+            return objectMapper.writeValueAsString(t);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    static public <T> T fromJson(String json, Class<T> tClass) {
+        try {
+            return objectMapper.readValue(json, tClass);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
