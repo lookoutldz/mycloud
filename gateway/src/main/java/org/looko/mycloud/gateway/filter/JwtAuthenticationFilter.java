a58e1b1b08e7a3b16db8fc6fb159aab0952f97c5
looko <lookoutldz@outlook.com>
2023-07-23 15:33:42 +0800

    优化 JwtAuthenticationFilter 的路径匹配逻辑

--- gateway/src/main/java/org/looko/mycloud/gateway/filter/JwtAuthenticationFilter.java
+++ gateway/src/main/java/org/looko/mycloud/gateway/filter/JwtAuthenticationFilter.java
@@ -7,15 +7,17 @@ import org.looko.mycloud.commonstarter.entity.ResponseEntity;
 import org.looko.mycloud.gateway.util.JwtUtil;
 import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.lang.NonNull;
+import org.springframework.security.web.server.util.matcher.OrServerWebExchangeMatcher;
+import org.springframework.security.web.server.util.matcher.PathPatternParserServerWebExchangeMatcher;
+import org.springframework.security.web.server.util.matcher.ServerWebExchangeMatcher;
 import org.springframework.web.server.ServerWebExchange;
 import org.springframework.web.server.WebFilter;
 import org.springframework.web.server.WebFilterChain;
 import reactor.core.publisher.Mono;
 
-import java.util.HashSet;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Set;
-import java.util.function.Predicate;
-import java.util.regex.Pattern;
 
 import static java.nio.charset.StandardCharsets.UTF_8;
 import static org.looko.mycloud.commonstarter.constant.AuthConstants.AUTH_HEADER_KEY;
@@ -24,42 +26,48 @@ import static org.looko.mycloud.commonstarter.enumeration.BasicResponseStatus.AU
 
 @Slf4j
 public class JwtAuthenticationFilter implements WebFilter {
-    private static final Set<Predicate<String>> whitePatterns;
+    private static final ServerWebExchangeMatcher exchangeMatcher;
     static {
         Set<String> whiteRegexes = Set.of("/resources/**",  "/auth/validcode/**", "/auth/resetPassword/**",
                 "/login", "/auth/register", "/user/test/port");
-        whitePatterns = new HashSet<>();
+        List<ServerWebExchangeMatcher> whitelistMatcher = new ArrayList<>(whiteRegexes.size());
         for (String regex : whiteRegexes) {
-            regex = regex.replaceAll("\\*", "\\\\*");
-            whitePatterns.add(Pattern.compile(regex).asMatchPredicate());
+            whitelistMatcher.add(new PathPatternParserServerWebExchangeMatcher(regex));
         }
+        exchangeMatcher = new OrServerWebExchangeMatcher(whitelistMatcher);
     }
 
     @Override
     @NonNull
     public Mono<Void> filter(@NonNull ServerWebExchange exchange, @NonNull WebFilterChain chain) {
-        String path = exchange.getRequest().getPath().value();
-        if (whitePatterns.stream().anyMatch(p -> p.test(path))) {
-            log.info("白名单[" + path + "]，跳过 jwt 校验");
-            return chain.filter(exchange);
-        }
+        return exchangeMatcher
+                .matches(exchange)
+                .flatMap(matchResult -> {
+                    String path = exchange.getRequest().getPath().value();
+                    if (matchResult.isMatch()) {
+                        log.info("JwtAuthentication 放行白名单 Path: {}", path);
+                        return chain.filter(exchange);
+                    } else {
+                        log.info("JwtAuthentication 拦截 Path: {}", path);
+                        String tokenHolder = exchange.getRequest().getHeaders().getFirst(AUTH_HEADER_KEY);
+                        String message = null;
+                        if (tokenHolder != null && tokenHolder.startsWith(TOKEN_PREFIX)) {
+                            String jwtToken = tokenHolder.replace(TOKEN_PREFIX, "");
+                            try {
+                                String tokenInfo = JwtUtil.verifyToken(jwtToken);
+                                log.info("Token 校验通过. Path[{}], token: {}", path, tokenInfo);
+                                return chain.filter(exchange);
+                            } catch (TokenExpiredException e) {
+                                message = "token 已失效，请重新登录!";
+                            } catch (JWTVerificationException e) {
+                                message = "token 验证失败!";
+                            }
+                        }
+                        ServerHttpResponse response = exchange.getResponse();
+                        return response.writeWith(Mono.just(response.bufferFactory().wrap(ResponseEntity.failure(AUTH_FAILED, message).toJson().getBytes(UTF_8))));
+                    }
+                });
 
-        String tokenHolder = exchange.getRequest().getHeaders().getFirst(AUTH_HEADER_KEY);
-        String message = null;
-        if (tokenHolder != null && tokenHolder.startsWith(TOKEN_PREFIX)) {
-            String jwtToken = tokenHolder.replace(TOKEN_PREFIX, "");
-            try {
-                String tokenInfo = JwtUtil.verifyToken(jwtToken);
-                log.info("Token 校验通过. Path[{}], token: {}", path, tokenInfo);
-                return chain.filter(exchange);
-            } catch (TokenExpiredException e) {
-                message = "token 已失效，请重新登录!";
-            } catch (JWTVerificationException e) {
-                message = "token 验证失败!";
-            }
-        }
-        ServerHttpResponse response = exchange.getResponse();
-        return response.writeWith(
-                Mono.just(response.bufferFactory().wrap(ResponseEntity.failure(AUTH_FAILED, message).toJson().getBytes(UTF_8))));
     }
+
 }
