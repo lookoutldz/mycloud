53a438b015742c99e020b288ccae615a427e6a7f
looko <lookoutldz@outlook.com>
2023-07-23 16:59:06 +0800

    更新一个状态码枚举

--- common-starter/src/main/java/org/looko/mycloud/commonstarter/enumeration/BasicResponseStatus.java
+++ common-starter/src/main/java/org/looko/mycloud/commonstarter/enumeration/BasicResponseStatus.java
@@ -2,10 +2,11 @@ package org.looko.mycloud.commonstarter.enumeration;
 
 public enum BasicResponseStatus implements ResponseStatus {
     SUCCESS(200, "🥳成功"),
-    SERVER_ERROR(500, "🥵出了点问题"),
+    SERVER_ERROR(500, "🤢出了点问题"),
     AUTH_FAILED(401, "🤯身份验证失败"),
     FORBIDDEN(403, "🫣权限不足"),
-    UNKNOWN_ERROR(999, "🤬未知错误");
+    TO_MANY_REQUEST(429, "🥵手速太快辣~请慢点"),
+    UNKNOWN_ERROR(999, "👽未知错误");
 
     private final Integer code;
     private final String message;
